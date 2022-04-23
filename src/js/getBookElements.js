138e344322bc10f75f9245277125bc2a832d86f5
Lijo <lijo.belardi@icloud.com>
2022-04-23 16:30:16 +0200

    Ottimizzata la ricerca degli autori

--- src/js/getBookElements.js
+++ src/js/getBookElements.js
@@ -13,26 +13,25 @@ export const getBookElements = async (books) => {
     books.map((book) => {
       let title = get(book, "title", "Title Unknown")
       let key = get(book, 'key', 'Key Unknown')
-      let authorsMainArray = get(book, 'authors', 'Authors Unknown')
-      //console.log(authorsMainArray);
+      let authors = get(book, 'authors', 'Authors Unknown')
+
+      if (Array.isArray(authors)) {
+        if (authors.length <= 4) {authors = authors.map((author) => {
+          return author.name;}).join(", ");
+        } else {
+          authors = `
+          ${authors[0].name}, 
+          ${authors[1].name}, 
+          ${authors[2].name}, 
+          ${authors[3].name} and others`;
+        }
+      }
       
-
-
-      //console.dir(singleBook);
-      let authors = authorsMainArray.map((item) => {
-        return item.name
-      })
-
-
-      //const singleBook = { key, title, authors }
-      //console.log(authors);
-      //console.log(singleBook);
-
       html += `
    
         <div class="book-container" data-id = "${key}">
             <div class="info-book">
-            <h2>Title: ${title}</h2>
+            <h2>${title}</h2>
             <p>Authors: ${authors}</p>
             </div>
         </div>
