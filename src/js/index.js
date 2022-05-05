88d251f090b04445ec5306a503ede5620b08d939
Lijo <lijo.belardi@icloud.com>
2022-05-05 14:56:45 +0200

    tolto codice inutile

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,6 @@
 import '../style/main.scss'; // connect scss to template.html
-import { getBooksByGenres } from './ApiRequests'
-import { check } from './displayBook';
 import { async } from '@babel/runtime/regenerator';
-import { log, logErrors, refreshFunction, submitFunction } from './utility';
+import { refreshFunction, submitFunction } from './utility';
 
 const form = document.getElementById('form')
 const refreshButton = document.getElementById('refresh-button')
